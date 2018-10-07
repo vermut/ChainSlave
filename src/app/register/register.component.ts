657d451f7d605f73c207a9cad78184362871ca99
Pavel Veretennikov <vermut@kid.lv>
2018-10-07 23:16:32 +0300

    reformat

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,33 +1,33 @@
-﻿import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+﻿import {Component} from '@angular/core';
+import {Router} from '@angular/router';
 
-import { AlertService, UserService } from '../_services/index';
+import {AlertService, UserService} from '../_services/index';
 
 @Component({
-    moduleId: module.id,
-    templateUrl: 'register.component.html'
+  templateUrl: 'register.component.html'
 })
 
 export class RegisterComponent {
-    model: any = {};
-    loading = false;
+  model: any = {};
+  loading = false;
 
-    constructor(
-        private router: Router,
-        private userService: UserService,
-        private alertService: AlertService) { }
+  constructor(
+    private router: Router,
+    private userService: UserService,
+    private alertService: AlertService) {
+  }
 
-    register() {
-        this.loading = true;
-        this.userService.create(this.model)
-            .subscribe(
-                data => {
-                    this.alertService.success('Registration successful', true);
-                    this.router.navigate(['login']);
-                },
-                error => {
-                    this.alertService.error(error);
-                    this.loading = false;
-                });
-    }
+  register() {
+    this.loading = true;
+    this.userService.create(this.model)
+      .subscribe(
+        data => {
+          this.alertService.success('Registration successful', true);
+          this.router.navigate(['login']);
+        },
+        error => {
+          this.alertService.error(error);
+          this.loading = false;
+        });
+  }
 }
