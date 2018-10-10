7050249b7256123d5d0057bb946e0f1c8379b9c8
Pavel Veretennikov <vermut@kid.lv>
2018-10-10 16:54:27 +0300

    geopush

--- src/pages/game/game.ts
+++ src/pages/game/game.ts
@@ -1,25 +1,70 @@
-import {Component, OnInit} from '@angular/core';
-import {IonicPage} from 'ionic-angular';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {IonicPage, Platform} from 'ionic-angular';
 import {GameData} from "../../app/_models";
 import {GamedataProvider} from "../../providers/gamedata/gamedata";
-import {timer} from "rxjs/observable/timer";
+import {Observable} from "rxjs";
+import {TimerObservable} from "rxjs/observable/TimerObservable";
+import {ISubscription} from "rxjs/Subscription";
 
-/**
- * Generated class for the GamePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+declare var jitsiplugin: any;
+declare var navigator: any;
+declare var cordova: any;
 
 @IonicPage()
 @Component({
   selector: 'page-game',
   templateUrl: 'game.html',
 })
-export class GamePage implements OnInit {
+export class GamePage implements OnInit, OnDestroy {
   gameData: GameData;
+  latitude = undefined;
+  longitude = undefined;
 
-  constructor(private gamedataProvider: GamedataProvider) {
+  private timer$: Observable<number>;
+  private $timer : ISubscription;
+
+  constructor(private platform: Platform, private gamedataProvider: GamedataProvider) {
+    platform.ready().then(() => {
+      console.log('Platform ready');
+
+      navigator.geolocation.getCurrentPosition((position) => {
+        console.log('Latitude: ' + position.coords.latitude + '\n' +
+          'Longitude: ' + position.coords.longitude + '\n' +
+          'Altitude: ' + position.coords.altitude + '\n' +
+          'Accuracy: ' + position.coords.accuracy + '\n' +
+          'Altitude Accuracy: ' + position.coords.altitudeAccuracy + '\n' +
+          'Heading: ' + position.coords.heading + '\n' +
+          'Speed: ' + position.coords.speed + '\n' +
+          'Timestamp: ' + position.timestamp + '\n');
+      }, (error) => {
+        console.log('code: ' + error.code + '\n' +
+          'message: ' + error.message + '\n');
+      }, {timeout: 5000, enableHighAccuracy: true});
+
+      navigator.geolocation.watchPosition(position => {
+        if (!position.coords) {
+          console.log("Empty position.coords?..");
+          return;
+        }
+
+        const updatedLatitude = position.coords.latitude;
+        const updatedLongitude = position.coords.longitude;
+
+        if (updatedLatitude != this.latitude && updatedLongitude != this.longitude) {
+          this.latitude = updatedLatitude;
+          this.longitude = updatedLongitude;
+
+          console.log('we are: ' + updatedLatitude + ", " + updatedLongitude);
+          this.gamedataProvider.putPosition(position).subscribe(_ => {}, error => {
+              console.log("this.gamedataProvider.putPosition(position) " + error);
+            }
+          );
+        }
+      }, (error) => {
+        console.log('code: ' + error.code + '\n' +
+          'message: ' + error.message + '\n');
+      }, {timeout: 5000, enableHighAccuracy: true});
+    });
   }
 
   ionViewDidLoad() {
@@ -27,10 +72,15 @@ export class GamePage implements OnInit {
   }
 
   ngOnInit(): void {
-    timer(0, 1000).subscribe(() =>
+    this.timer$ = TimerObservable.create(0, 1000);
+    this.$timer = this.timer$.subscribe(_ =>
       this.gamedataProvider.getData().subscribe(data => {
           this.gameData = data;
         }
       ));
+  }
+
+  ngOnDestroy(): void {
+    this.$timer.unsubscribe();
   }
 }
--- src/providers/gamedata/gamedata.ts
+++ src/providers/gamedata/gamedata.ts
@@ -1,5 +1,5 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
 import {GameData} from "../../app/_models";
 
 /*
@@ -18,6 +18,15 @@ export class GamedataProvider {
   getData() {
     return this.http.get<GameData>(SERVER_URL + '/api/gamedata', {withCredentials: true});
   }
+
+  putPosition(position: Position) {
+    return this.http.put<Position>(SERVER_URL + '/api/gamedata', {
+      timestamp: position.timestamp,
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+    }, {withCredentials: true});
+  }
 }
 
+// export const SERVER_URL = "https://chainreactor.herokuapp.com";
 export const SERVER_URL = "http://192.168.118.63:8080";
