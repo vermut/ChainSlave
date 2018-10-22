10624556df8d616a126bc02f8ee46ca383663a33
Pavels Veretennikovs <PVeretennikovs@riskfocus.com>
2018-10-22 11:25:47 +0300

    Update package-android.sh

--- travis/package-android.sh
+++ travis/package-android.sh
@@ -8,6 +8,5 @@ then
     exit
 fi
 
-mkdir -p output
-cp platforms/android/build/outputs/apk/android-release-unsigned.apk output/ionictravis-release-unsigned.apk
-
+mkdir -p output 
+cp platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk output/chainslave-release-unsigned.apk
