f6b54d571187ea0ea245b3690753b175f04de259
Pavels Veretennikovs <PVeretennikovs@riskfocus.com>
2018-10-22 17:19:53 +0300

    --device

--- travis/build-ios.sh
+++ travis/build-ios.sh
@@ -3,11 +3,11 @@
 set -e
 
 # Build Ionic App for iOS
-ionic cordova platform add ios --verbose
+ionic cordova platform add ios
 
 if [[ "$TRAVIS_BRANCH" == "develop" ]]
 then
-    ionic cordova build ios
+    ionic cordova build ios --device
 else
-    ionic cordova build ios --prod --release
+    ionic cordova build ios --prod --release --device
 fi
