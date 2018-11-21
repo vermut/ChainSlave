9ec7422ae90ddf7b291c136d58a96f2440febb3a
Ilya Kirichek <mnillcoway@gmail.com>
2018-11-21 15:01:41 +0300

    Сделал вывод ошибок, потом надо посмотреть в каком формате описание ошибки выдет бекенд, чтобы сделать их более юзеро-читабельными.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -35,7 +35,7 @@ export class LoginComponent implements OnInit {
           this.router.navigate([this.returnUrl]);
         },
         error => {
-          this.alertService.error(error);
+          this.alertService.error(error && error.message || error || "Internal error");
           this.loading = false;
         });
   }
